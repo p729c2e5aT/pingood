808dfe2bf04e75119c7a94d89ef4fdb03811212b
akane.simauma <daphne.78216simaoki@gmail.com>
2019-11-21 16:12:49 +0900

    画像の表示方法変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Needed for Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
