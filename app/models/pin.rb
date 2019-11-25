32e6d2338c85368bb6a786acba1ea4e8d3c3a000
akane.simauma <daphne.78216simaoki@gmail.com>
2019-11-25 17:00:23 +0900

    環境変数再設定

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,9 +4,8 @@ class Pin < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "300x300>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/,
-  :storage => :s3,
-  :s3_permissions  => :public,
-  :s3_credentials  => "#{Rails.root}/config/s3.yml",
   :path => ":attachment/:id/:style.:extension"
-  
+  default_url: "/images/:style/missing.png" 
+
+  Paperclip::Attachment.default_options[:url] = 'pingood.senn.s3.amazonaws.com'
 end
