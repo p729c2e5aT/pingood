0def000d94dff7985b4c3dc21c90ebad2bde537d
akane.simauma <daphne.78216simaoki@gmail.com>
2019-11-25 15:45:47 +0900

    modelの記述変更

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,10 +3,10 @@ class Pin < ApplicationRecord
   belongs_to :user
 
   has_attached_file :image, styles: { medium: "300x300>"}
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-  storage: :s3,
-  s3_permissions: :public,
-  s3_credentials: "#{Rails.root}/config/s3.yml",
-  path: ":attachment/:id/:style.:extension"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/,
+  :storage => :s3,
+  :s3_permissions  => :public,
+  :s3_credentials  => "#{Rails.root}/config/s3.yml",
+  :path => ":attachment/:id/:style.:extension"
   
 end
