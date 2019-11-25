8e6d6721e0287c320771f37c730778b145729c22
akane.simauma <daphne.78216simaoki@gmail.com>
2019-11-25 17:04:35 +0900

    Update pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,9 +2,9 @@ class Pin < ApplicationRecord
   acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>"}
+  has_attached_file :image, styles: { medium: "300x300>"},git push heroku master
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/,
-  :path => ":attachment/:id/:style.:extension"
+  :path => ":attachment/:id/:style.:extension",
   default_url: "/images/:style/missing.png" 
 
   Paperclip::Attachment.default_options[:url] = 'pingood.senn.s3.amazonaws.com'
