e6f178b3dd492396c1aa104e399546f2b68ec22b
akane.simauma <daphne.78216simaoki@gmail.com>
2019-11-25 17:08:35 +0900

    Update pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,7 @@ class Pin < ApplicationRecord
   acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>"},git push heroku master
+  has_attached_file :image, styles: { medium: "300x300>"},
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/,
   :path => ":attachment/:id/:style.:extension",
   default_url: "/images/:style/missing.png" 
