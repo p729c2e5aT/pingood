6c2a977ed3e66d5e24bb4c60448ef1925b449f6c
akane.simauma <daphne.78216simaoki@gmail.com>
2019-11-25 17:14:56 +0900

    update

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,10 +2,9 @@ class Pin < ApplicationRecord
   acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>"},
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/,
+  has_attached_file :image,
   :path => ":attachment/:id/:style.:extension",
-  default_url: "/images/:style/missing.png" 
-
-  Paperclip::Attachment.default_options[:url] = 'pingood.senn.s3.amazonaws.com'
+  styles: { medium: "300x300>"},
+  default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
